955edac6b8ec3deca4c1df739e29749a98faa333
coffee <coffeehc@gmail.com>
2016-11-13 00:21:30 +0800

    .

--- httpcommons/httpclient/client.go
+++ httpcommons/httpclient/client.go
@@ -3,6 +3,7 @@ package httpclient
 import (
 	"time"
 	"github.com/valyala/fasthttp"
+	"errors"
 )
 
 type _HttpClient struct {
@@ -19,14 +20,36 @@ func (this *_HttpClient)GetDeadline(dst []byte, url string, deadline time.Time) 
 	return this.client.GetDeadline(dst,url,deadline)
 }
 func (this *_HttpClient)Post(dst []byte, url string, postArgs HttpArgs) (statusCode int, body []byte, err error){
-	return this.client.Post(dst,url,postArgs.getFastHttpAtgs())
+	var args *fasthttp.Args
+	if postArgs != nil {
+		args = postArgs.getFastHttpAtgs()
+	}
+	return this.client.Post(dst, url,args)
 }
 func (this *_HttpClient)DoTimeout(req HttpRequest, resp HttpResponse, timeout time.Duration) error{
+	if req == nil{
+		return errors.New("Request 不能为 nil")
+	}
+	if resp == nil{
+		return errors.New("Request 不能为 nil")
+	}
 	return this.client.DoTimeout(req.getFastHttpRequest(),resp.getFastHttpResponse(),timeout)
 }
 func (this *_HttpClient)DoDeadline(req HttpRequest, resp HttpResponse, deadline time.Time) error{
+	if req == nil{
+		return errors.New("Request 不能为 nil")
+	}
+	if resp == nil{
+		return errors.New("Request 不能为 nil")
+	}
 	return this.client.DoDeadline(req.getFastHttpRequest(),resp.getFastHttpResponse(),deadline)
 }
 func (this *_HttpClient)Do(req HttpRequest, resp HttpResponse) error{
+	if req == nil{
+		return errors.New("Request 不能为 nil")
+	}
+	if resp == nil{
+		return errors.New("Request 不能为 nil")
+	}
 	return this.client.Do(req.getFastHttpRequest(),resp.getFastHttpResponse())
 }
