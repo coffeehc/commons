a3e37405074d08a5d0220ee7e0d710e36dd5248a
coffee <coffee@xxx>
2023-04-25 16:54:02 +0800

    20230425 修改了加密参数的返回为[]byte

--- cryptos/encoder.go
+++ cryptos/encoder.go
@@ -2,20 +2,18 @@ package cryptos
 
 import (
 	"crypto/aes"
-	"encoding/base64"
-
 	"github.com/coffeehc/base/errors"
 	"github.com/coffeehc/base/log"
 	"go.uber.org/zap"
 )
 
-func DataEncode(data []byte, mask DataMasker) (string, error) {
+func DataEncode(data []byte, mask DataMasker) ([]byte, error) {
 	key := GetRandBytes(16)
 	vi := GetRandBytes(aes.BlockSize)
 	dist, err := AesCBCEncrypt(data, key, vi, PKCS7_PADDING)
 	if err != nil {
 		log.Warn("AESEncrypt失败", zap.Error(err))
-		return "", errors.ConverError(err)
+		return nil, errors.ConverError(err)
 	}
 	if mask != nil {
 		mask.Mark(key)
@@ -29,15 +27,15 @@ func DataEncode(data []byte, mask DataMasker) (string, error) {
 	copy(tokenData[4+distLen:], key[4:12])
 	copy(tokenData[12+distLen:], dist[distLen:])
 	copy(tokenData[12+distLen*2:], key[12:])
-	return base64.RawStdEncoding.EncodeToString(sourceData), nil
+	return sourceData, nil
 }
 
-func DataDecode(token string, mask DataMasker) ([]byte, error) {
-	sourceData, err := base64.RawStdEncoding.DecodeString(token)
-	if err != nil {
-		log.Warn("解析Token失败", zap.Error(err))
-		return nil, errors.MessageError("token非法")
-	}
+func DataDecode(sourceData []byte, mask DataMasker) ([]byte, error) {
+	//sourceData, err := base64.RawStdEncoding.DecodeString(token)
+	//if err != nil {
+	//	log.Warn("解析Token失败", zap.Error(err))
+	//	return nil, errors.MessageError("token非法")
+	//}
 	dist := sourceData[4:]
 	key := make([]byte, 16)
 	data := make([]byte, len(dist)-16)
@@ -51,7 +49,7 @@ func DataDecode(token string, mask DataMasker) ([]byte, error) {
 		mask.UnMark(key)
 	}
 
-	data, err = AesCBCDecrypt(data[aes.BlockSize:], key, data[:aes.BlockSize], PKCS7_PADDING)
+	data, err := AesCBCDecrypt(data[aes.BlockSize:], key, data[:aes.BlockSize], PKCS7_PADDING)
 	if err != nil {
 		log.Warn("Token解码失败", zap.Error(err))
 		return nil, errors.MessageError("token非法")
